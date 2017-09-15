ef65583d4ddef9c18b507323be3a153d859b3cca
Luca <luca@rvd.li>
2017-09-15 16:42:38 +0200

    Initial commit

---
+++ src/main/java/de/qvbe/basics/Main.java
@@ -0,0 +1,19 @@
+package de.qvbe.basics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by luca on 15.09.17.
+ */
+public class Main extends JavaPlugin {
+
+    private static Main main;
+
+    
+
+
+    public static Main get() {
+        return main;
+    }
+
+}
